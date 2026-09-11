4e2f63b2abf9964d5680c1adabed06b640f37bea
lucasjanta <lucastyle2014@gmail.com>
2026-09-10 23:23:08 -0300

    ring logic

--- scenes/actors/player.gd
+++ scenes/actors/player.gd
@@ -11,7 +11,8 @@ var dir : float
 @export var jump_force : float = -250.0
 @export var gravity_force : float = 850.0
 @export var dash_speed : float = 300.0
-@export var equipped_rings : Array[Ring]
+
+@export var equipped_rings : Array[Ring] = []
 @export var bag_rings : Array[Ring]
 
 @export var max_hp : float = 100.0
@@ -44,3 +45,32 @@ func take_damage(dmg):
 	player_hud.update_hp()
 	if hp <= 0.0:
 		print("player died")
+
+func equip_ring(ring: Ring) -> void:
+	if equipped_rings.size() >= 2:
+		return
+
+	equipped_rings.append(ring)
+	ring.apply_passive(self)
+
+
+func unequip_ring(index: int) -> void:
+	if index < 0 or index >= equipped_rings.size():
+		return
+
+	var ring := equipped_rings[index]
+
+	ring.remove_passive(self)
+	equipped_rings.remove_at(index)
+	
+func use_ring(index: int) -> void:
+	if index < 0 or index >= equipped_rings.size():
+		return
+
+	var ring := equipped_rings[index]
+
+	if not ring.can_use():
+		return
+
+	ring.use(self)
+	ring.consume_use()
--- scripts/resources/ring_base.gd
+++ scripts/resources/ring_base.gd
@@ -2,6 +2,33 @@ extends Resource
 class_name Ring
 
 @export var ring_name : String
-@export var ring_max_durability : int = 0
-@export var actual_durability : int = 0
-@export var ring_experience : float = 0.0
+@export var max_uses: int = 20
+@export var uses: int = 20
+@export var texture : Texture2D
+
+# 0 = Basic
+# 1 = Experienced
+# 2 = Ancient
+@export_range(0, 2) var power_stage: int = 0
+
+func use(player: Player) -> void:
+	pass
+
+
+func apply_passive(player: Player) -> void:
+	pass
+
+
+func remove_passive(player: Player) -> void:
+	pass
+
+
+func can_use() -> bool:
+	return uses > 0
+
+
+func consume_use() -> void:
+	uses -= 1
+
+	if uses <= 0:
+		uses = 0
